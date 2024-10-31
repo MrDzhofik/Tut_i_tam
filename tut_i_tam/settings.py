f2640b776e540a2dbfbe73e33a5b177d43cee856
MrDzhofik <djafarovemil04@mail.ru>
2024-10-31 17:33:13 +0300

    Починил статику, теперь статика приложений в своей директории

--- tut_i_tam/settings.py
+++ tut_i_tam/settings.py
@@ -117,9 +117,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = BASE_DIR / 'all_staticfiles'
+STATICFILES_DIRS = [
+    # os.path.join(BASE_DIR, 'static')
+    BASE_DIR / 'tour' / 'static',
+    BASE_DIR / 'static',
+    
+    ]
+
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
